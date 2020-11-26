856832513dbe616a0c41528b7ae99c5f89f8405c
varunraj30 <72302361+varunraj30@users.noreply.github.com>
2020-11-26 15:14:09 +0530

    docs: UPDATED index.js
    It is a Quiz About Marvel And It is Based on CLI using NODEJS.

--- index.js
+++ index.js
@@ -9,16 +9,11 @@ console.log("This Quiz is About How well you know Marvel.")
 var def = readlineSync.question("So Are You Ready? ")
 if (def.toLowerCase() == "yes") {
   console.log("Let the Game Begin")
-}
-else {
-  console.log("Take your time");
-}
-
-function quiz(question, answer) {
+  function quiz(question, answer) {
   var ranswer = readlineSync.question(question);
 
   if (ranswer.toUpperCase() === answer.toUpperCase()) {
-    console.log("Right Answer is ", answer)
+    console.log("Right Answer! ")
     score += 1;
     console.log("Score is ", score)
 
@@ -30,10 +25,12 @@ function quiz(question, answer) {
 }
 
 quiz("What is Iron Man's Reel Name? ", "tony stark");
-quiz("Did Iron Man killed all the avengers? ", "no");
-quiz("Iron Man Dies in which Marvel Movie? ", "Avengers End Game");
+quiz("Who does the Mad Titan sacrifice to acquire the Soul Stone? ", "Gamora");
+quiz("Iron Man Dies in which Marvel Movie? ", "Avengers Endgame");
+quiz("What is the name of Thor’s hammer? ","Mjolnir");
+quiz("Who is killed by Loki in the Avengers? ","Agent Coulson");
 console.log("Final Score is ", score)
-if (score == 3) {
+if (score == 5) {
   console.log("You are Quite a Marvel Fan, Well I am Too You can proceeded to next level.Type yes to continue or no to exit")
   var nxtlvl = readlineSync.question();
   if (nxtlvl.toLowerCase() === "yes") {
@@ -41,11 +38,17 @@ if (score == 3) {
     quiz("What is Chris Evans Name in Marvel Universe? ", "Steve Rogers")
     quiz("Will Black Window die in Infinity war? ", "no")
     quiz("Thanos was first seen in which Marvel Movie? ", "Guardians of Galaxy")
-    if (score == 6) {
+    quiz("What is Captain America’s shield made of? ","Vibranium");
+    quiz("Which of the infinity stones is hidden on Vormir?","Soul Stone");
+    if (score == 10) {
       console.log("Quiz is Done.Your a crazy Marvel Fan.")
     }
+    if(score<=5) {
+      console.log("Your score is below 5.Better luck next time")
+    }
     else {
-      console.log("You Many not have answered all questions correctly.Better luck next time")
+      console.log("Score is",score)
+      console.log("Better luck next time")
     }
   }
   if (nxtlvl === "no") {
@@ -55,3 +58,8 @@ if (score == 3) {
 else {
   console.log("Better Luck Next Time!")
 }
+}
+else {
+  console.log("Take your time");
+}
+
